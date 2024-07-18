2b7870bea67edbeb02e8838ad65b74ea117d3a42
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-18 08:36:16 +0000

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ src/test/java/com/cn/mock/service/Categories.java
@@ -0,0 +1,61 @@
+package com.cn.mock.service;
+
+public interface Categories {
+
+	interface getService {
+
+	}
+
+	interface roostTestTag1 {
+
+	}
+
+	interface roostTestTag2 {
+
+	}
+
+	interface getRes {
+
+	}
+
+	interface test {
+
+	}
+
+	interface getDesc {
+
+	}
+
+	interface publicStaticMethod {
+
+	}
+
+	interface publicMethod {
+
+	}
+
+	interface get {
+
+	}
+
+	interface doSomethingReturnBoolean {
+
+	}
+
+	interface m1 {
+
+	}
+
+	interface m2 {
+
+	}
+
+	interface canExecute {
+
+	}
+
+	interface isSameDay {
+
+	}
+
+}
